31c86a70d0e9c66f89c26c10bd9df6bf0801e285
Akshar Bisht <bisht.akshar@gmail.com>
2023-04-09 08:26:27 +0530

    fixed typo

--- checkpoint1.playground/Contents.swift
+++ checkpoint1.playground/Contents.swift
@@ -2,4 +2,4 @@ import Cocoa
 
 let celcius = 36.0
 let fahrenheit = (celcius * 9) / 5 + 32
-print("The temperature is \(celcius)° Celius or \(fahrenheit) Fahrenheit")
+print("The temperature is \(celcius)° Celsius or \(fahrenheit) Fahrenheit")
