caa15ed626961fbd203871dd259ddc6af8933b55
Akshar Bisht <bisht.akshar@gmail.com>
2023-04-18 22:48:05 +0530

    loops continued

--- day6.playground/Contents.swift
+++ day6.playground/Contents.swift
@@ -6,5 +6,14 @@ for fruit in fruits {
 }
 
 for i in 1...10 {
-    print("5 x \(i) = \(5*i)")
+    print("5 x \(i) = \(5*i)") //multiplication table of 5
+}
+
+//multiplication tables from 1 to 10:
+for i in 1...10 {
+    print("Multiplication table of \(i):")
+    for j in 1...10 {
+        print("\(j) x \(i) = \(j*i)")
+    }
+              print()
 }
