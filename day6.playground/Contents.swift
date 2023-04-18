77a6339077ffef41993cc20b7401ab40b00fd39e
Akshar Bisht <bisht.akshar@gmail.com>
2023-04-18 22:44:43 +0530

    for loop contd

--- day6.playground/Contents.swift
+++ day6.playground/Contents.swift
@@ -4,3 +4,7 @@ let fruits = ["apple", "banana", "watermelon", "kiwi", "pear", "papaya"]
 for fruit in fruits {
     print("\(fruit) is a fruit")
 }
+
+for i in 1...10 {
+    print("5 x \(i) = \(5*i)")
+}
