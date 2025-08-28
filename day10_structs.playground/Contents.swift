16e39d63c44a932634a6d22a5a6a2c1ce9deb8cc
Akshar Bisht <bisht.akshar@gmail.com>
2025-08-28 15:46:15 +0530

    mutating struct

--- day10_structs.playground/Contents.swift
+++ day10_structs.playground/Contents.swift
@@ -14,3 +14,22 @@ let mainCar = Cars(name: "Civic", brand: "Honda", year: 2019)
 let secondaryCar = Cars(name: "A4", brand: "Audi", year: 2018)
 mainCar.printSummary()
 secondaryCar.printSummary()
+
+struct Employee {
+    let name: String
+    var vacationRemaining: Int
+    
+    mutating func takeVacation(days: Int) {
+        if vacationRemaining >= days {
+            vacationRemaining -= days
+            print("You can go on vacation!")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("You don't have any vacation days remainig!")
+        }
+    }
+}
+
+var employee1 = Employee(name: "John", vacationRemaining: 10)
+employee1.takeVacation(days: 5)
+
